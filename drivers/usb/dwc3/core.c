859b65de08d36d9643a1b477972f85da8a2f95d9
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-07-17 17:09:25 +0700

    drivers/usb: dwc3: Silence warning when IPC logging is disable
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1388,9 +1388,10 @@ static int dwc3_probe(struct platform_device *pdev)
 					dev_name(dwc->dev), 0);
 	if (!dwc->dwc_ipc_log_ctxt)
 		dev_err(dwc->dev, "Error getting ipc_log_ctxt\n");
-
+#endif
 	snprintf(dma_ipc_log_ctx_name, sizeof(dma_ipc_log_ctx_name),
 					"%s.ep_events", dev_name(dwc->dev));
+#ifdef CONFIG_IPC_LOGGING
 	dwc->dwc_dma_ipc_log_ctxt = ipc_log_context_create(NUM_LOG_PAGES,
 						dma_ipc_log_ctx_name, 0);
 	if (!dwc->dwc_dma_ipc_log_ctxt)
