aeab96006a5a0228030c18935e7667369f35c4bc
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-07-24 17:14:55 +0700

    schedutil: Apply init protection for configure tunables
    Most custom ROMs on ginkgo device already have tunables
    configured in shedutil, so block init in order to use
    tunable configuration from kernel.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -18,6 +18,7 @@
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
 #include <linux/battery_saver.h>
+#include <linux/binfmts.h>
 #include "sched.h"
 
 #define SUGOV_KTHREAD_PRIORITY	50
@@ -570,6 +571,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -589,6 +593,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
