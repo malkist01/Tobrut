5e98cdea71220bc6e7d7fd2ee5d9c4ff45cbb100
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-01-05 23:44:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ Time: <code>$(date)</code>"
 # ===== CLANG =====
 if ! [ -d "${CLANG_DIR}" ]; then
 tg_msg "⚙️ Cloning Clang..."
-git clone --depth=1 https://github.com/malkist01/clang22 ${CLANG_DIR} || {
+git clone --depth=1 https://gitlab.com/kei-space/clang/r547379 ${CLANG_DIR} || {
 tg_msg "❌ <b>Failed cloning Clang</b>"
 }
 fi
